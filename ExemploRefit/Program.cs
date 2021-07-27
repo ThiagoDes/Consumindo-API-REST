7084f12dd12c8e6a450d79434c35ceaf59f38820
ThiagoDes008 <thiago.gomes@fusiondms.com.br>
2021-07-27 09:31:18 -0300

    Iniciando projeto

---
+++ ExemploRefit/Program.cs
@@ -0,0 +1,34 @@
+﻿using Refit;
+using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ExemploRefit
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var cepClient = RestService.For<ICepApiService>("https://viacep.com.br");
+
+                Console.WriteLine("Seja bem vindo!");
+                Console.WriteLine("Informe seu Cep:");
+
+                String cepInformado = Console.ReadLine().ToString();
+                cepInformado = Regex.Replace(cepInformado, "[^0-9a-zA-Z]+", "");
+                Console.WriteLine("Consultando informações do CEP: " + cepInformado);
+
+                var address = await cepClient.GetAddressAsync(cepInformado);
+
+                Console.WriteLine($"\nLogradouro: {address.Logradouro}, \nBairro: {address.Bairro}, \nCidade: {address.Localidade}");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro na consulta de cep: " + ex.Message);
+            }
+        }
+    }
+}
